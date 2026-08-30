9d2ce9712b4ee9e45f86758648b38db0c7aae0f2
The Soccer Goals <www.solsof.com@gmail.com>
2026-08-30 07:45:58 +0500

    Set league results page title to 'League Name Match Results Year Today'

--- resources/views/results/show.blade.php
+++ resources/views/results/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.site')
 
-@section('title', $league->name . ' Results ' . $league->season)
+@section('title', $league->name . ' Match Results ' . now()->year . ' Today')
 @section('meta_description', 'Every ' . $league->name . ' result so far this season, with full match reports and stats for each game.')
 @section('meta_keywords', $league->name . ' results, ' . $league->name . ' scores, final scores, ' . $league->country . ' football results')
 @section('canonical', route('results.show', $league->slug))
